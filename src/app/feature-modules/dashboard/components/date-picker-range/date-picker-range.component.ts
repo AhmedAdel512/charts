54c2350654d633a0b5f28f4eca27a99901799b60
ahmed-hefnyy <ahmed.hefny@flairstech.com>
2022-04-19 05:27:09 +0200

    [Enhancement] enhancement in date picker range component

--- src/app/feature-modules/dashboard/components/date-picker-range/date-picker-range.component.ts
+++ src/app/feature-modules/dashboard/components/date-picker-range/date-picker-range.component.ts
@@ -1,27 +1,22 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-date-picker-range',
   templateUrl: './date-picker-range.component.html',
   styleUrls: ['./date-picker-range.component.css']
 })
-export class DatePickerRangeComponent implements OnInit {
+export class DatePickerRangeComponent {
 
   @Output() public startDate = new EventEmitter<string>();
   @Output() public endDate = new EventEmitter<string>();
-  public from = null;
-  public to = null;
-
+  @Input() label: string = 'Enter a date Range'
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   getStartDate(value) {
     this.startDate.emit(value);
   }
   getEndDate(value) {
-    this.endDate.emit(value);    
+    this.endDate.emit(value);
   }
 
 }
