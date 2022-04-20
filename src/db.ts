896ca04e1fa31d0e4459bde3e60b7d34835fc357
ahmed-hefnyy <ahmed.hefny@flairstech.com>
2022-04-20 17:02:51 +0200

    [ADD] added more dummy data

--- src/db.ts
+++ src/db.ts
@@ -25,3 +25,378 @@ export const CLIENTS = {
       }
    ],
 }
+
+export const ACCOUNTS = [
+   {
+      client: 'FlairsTech',
+      portfolios: {
+         labels: ['2016', '2017', '2018', '2019', '2022'],
+         datasets: [
+            { data: [16, 25, 18, 24, 22], label: 'Front End' },
+            { data: [10, 16, 15, 23, 28], label: ' Back End' },
+            { data: [13, 17, 20, 19, 15], label: 'Testing' },
+            { data: [8, 10, 12, 16, 10], label: 'BA', backgroundColor: '#82E0AA', hoverBackgroundColor: '#58D68D' },
+         ],
+      },
+      info: [
+         {
+            title: 'Total Resources',
+            value: 55,
+            // bgColor: '#FDD835',
+            bgColor: 'bg-gradient-danger',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Revenue',
+            value: '20,301.56',
+            bgColor: 'bg-gradient-danger',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Active Projects',
+            value: '20 / 120',
+            bgColor: 'bg-gradient-danger',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Active Tickets',
+            value: '108 / 560',
+            bgColor: 'bg-gradient-danger',
+            textColor: 'text-white',
+         },
+      ],
+      employees: [
+         {
+            title: 'Back End',
+            fullName: 'Mohamed Ahmed Hassan',
+            email: 'mohamed@flairstech.com',
+            idle: 1,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Testing',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'BA',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+
+      ],
+      accountInfoByYear: [
+         {
+            year: 2016,
+            boxes: [
+               {
+                  title: 'Total Resources',
+                  value: 15,
+                  bgColor: '#F1C40F',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Revenue',
+                  value: '5,280.56',
+                  bgColor: '#F39C12',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Projects',
+                  value: '2 / 120',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Tickets',
+                  value: '56 / 560',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+            ]
+         },
+         {
+            year: 2017,
+            boxes: [
+               {
+                  title: 'Total Resources',
+                  value: 15,
+                  bgColor: '#F1C40F',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Revenue',
+                  value: '3,131.50',
+                  bgColor: '#F39C12',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Projects',
+                  value: '8 / 120',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Tickets',
+                  value: '70 / 560',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+            ]
+         },
+      ]
+   },
+   {
+      client: 'ITWorx',
+      portfolios: {
+         labels: ['2012', '2011', '2010', '2015', '2023'],
+         datasets: [
+            { data: [12, 25, 18, 24, 22], label: 'Front End' },
+            { data: [10, 16, 15, 23, 28], label: ' Back End' },
+            { data: [13, 17, 20, 19, 15], label: 'Testing' },
+            { data: [8, 10, 12, 16, 10], label: 'BA', backgroundColor: '#82E0AA', hoverBackgroundColor: '#58D68D' },
+         ],
+      },
+      info: [
+         {
+            title: 'Total Resources',
+            value: 55,
+            // bgColor: '#FDD835',
+            bgColor: 'bg-gradient-teal',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Revenue',
+            value: '20,301.56',
+            bgColor: 'bg-gradient-teal',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Active Projects',
+            value: '20 / 120',
+            bgColor: 'bg-gradient-teal',
+            textColor: 'text-white',
+         },
+         {
+            title: 'Active Tickets',
+            value: '108 / 560',
+            bgColor: 'bg-gradient-teal',
+            textColor: 'text-white',
+         },
+      ],
+      employees: [
+         {
+            title: 'Back End',
+            fullName: 'Mohamed Ahmed Hassan',
+            email: 'mohamed@flairstech.com',
+            idle: 1,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Testing',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'BA',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2016-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 0,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+         {
+            title: 'Mid-Level Front End',
+            fullName: 'Ahmed Hefny Gad',
+            email: 'ahmed@flairstech.com',
+            idle: 1,
+            date: '2022-02-17T22:00:00.000Z',
+         },
+
+      ],
+      accountInfoByYear: [
+         {
+            year: 2016,
+            boxes: [
+               {
+                  title: 'Total Resources',
+                  value: 15,
+                  bgColor: '#F1C40F',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Revenue',
+                  value: '5,280.56',
+                  bgColor: '#F39C12',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Projects',
+                  value: '2 / 120',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Tickets',
+                  value: '56 / 560',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+            ]
+         },
+         {
+            year: 2017,
+            boxes: [
+               {
+                  title: 'Total Resources',
+                  value: 15,
+                  bgColor: '#F1C40F',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Revenue',
+                  value: '3,131.50',
+                  bgColor: '#F39C12',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Projects',
+                  value: '8 / 120',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+               {
+                  title: 'Active Tickets',
+                  value: '70 / 560',
+                  bgColor: '#BDC3C7',
+                  textColor: 'text-white',
+               },
+            ]
+         },
+      ]
+   }
+]
